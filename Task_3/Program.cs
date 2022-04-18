1c78f2f57d1ad0c0941e29918d6a1defd810d2e5
Fedor <fedor_prg@rambler.ru>
2022-04-18 20:14:35 +1000

    Task 3 ready

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int index = 1;
+
+while (index <= a - 1)
+{
+    Console.Write($"{Math.Pow(index, 3)}, ");
+    index++;
+}
+Console.Write($"{Math.Pow(index, 3)}.");
