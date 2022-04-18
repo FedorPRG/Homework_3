83788fd77d7a5402e135d9868d11dfc56b0badf9
Fedor <fedor_prg@rambler.ru>
2022-04-18 21:17:20 +1000

    Task_1 ready

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите пятизначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a / 10000 == 0)
+{
+    Console.WriteLine("Вы ввели не пятизначное число!");
+}
+else
+{
+    int number1 = a % 10;
+    int number2 = (a / 10) % 10;
+    int number4 = (a / 1000) % 10;
+    int number5 = (a / 10000) % 10;
+    if (number1 == number5 & number2 == number4)
+    {
+        Console.WriteLine("Введеное число палиндром");
+    }
+    else Console.WriteLine("Введеное число не палиндром");
+}
